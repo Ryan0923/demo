de093e993c8bf910096a96806b2a4848012793cb
mojunbin <mojunbin@qq.com>
2016-03-06 15:53:08 +0800

    btrace demo用例

--- btrace/src/main/java/com/alibaba/mojb/case2/ExpandCapacityBtrace.java
+++ btrace/src/main/java/com/alibaba/mojb/case2/ExpandCapacityBtrace.java
@@ -1,14 +1,11 @@
 package com.alibaba.mojb.case2;
 
 import com.sun.btrace.AnyType;
-import com.sun.btrace.BTraceUtils;
 import com.sun.btrace.annotations.*;
 
-import java.lang.reflect.Field;
 import java.util.Map;
 
 import static com.sun.btrace.BTraceUtils.*;
-import static com.sun.btrace.BTraceUtils.println;
 
 /**
  * Created by mojunbin on 2016/3/5.
@@ -20,13 +17,17 @@ public class ExpandCapacityBtrace {
     @OnMethod(clazz = "java.util.HashMap", method = "resize",
             location = @Location(value = Kind.CALL, clazz = "/.*/", method = "/.*/"))
     public static void traceMapExpandCapacity(@ProbeClassName String probeClass, @ProbeMethodName String probeMethod,
-                                              @Self java.util.HashMap self) {
-        String point = Strings.strcat(Strings.strcat(probeClass, "."), probeMethod);
+                                              @Self Object self,int newCapacity) {
+        String point = Strings.strcat(Strings.strcat(probeClass, "."), probeMethod);//java/util/HashMap.resize
+        Class clazz = classForName("java.util.HashMap");
         println(Strings.strcat(point, "========================================================================"));
-        Map.Entry[] entry = (Map.Entry[]) get(field("java.util.HashMap", "table"), self);
+        println(Strings.strcat("clazz:", str(clazz)));
+        print(newCapacity);
         //获取实例protected变量
-        Strings.strcat("threshold:", get(field("java.util.HashMap", "threshold"), self).toString());
-        Strings.strcat("entry:", entry.toString());
+        Map.Entry[] entry = (Map.Entry[]) get(field(clazz, "table", true), self);
+        int threshold = getInt(field(clazz, "threshold", true), self);
+        println(Strings.strcat("threshold:", str(threshold)));
+        println(Strings.strcat("entry.length:", str(entry.length)));
         println(Strings.strcat(point, "-------------------------------------------------------------------------"));
     }
 }
