623936fdf35c52be54596ed39bba613262b571c6
mojunbin <mojunbin@qq.com>
2016-06-14 01:49:50 +0800

    FULLGC

---
+++ jvm/src/main/java/FullGc.java
@@ -0,0 +1,25 @@
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class FullGc {
+
+    private static final Collection<Object> leak = new ArrayList<Object>();
+    private static volatile Object sink;
+
+    // Run with: -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCDetails
+    // Notice that all the stop the world pauses coincide with GC pauses
+
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                leak.add(new byte[1024 * 1024]);
+
+                sink = new byte[1024 * 1024];
+            } catch (OutOfMemoryError e) {
+                leak.clear();
+            }
+        }
+    }
+}
