036cbb9d1ab85a8add1f8b32f3185f475e267308
mojunbin <mojunbin@qq.com>
2017-10-18 00:20:55 +0800

    线程池复习

--- base/src/main/java/queue/DelayQueueMain.java
+++ base/src/main/java/queue/DelayQueueMain.java
@@ -1,21 +1,28 @@
 package queue;
 
-import java.util.concurrent.DelayQueue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.CollectionUtils;
 
 /**
  * Created by mojunbin on 2016/3/21.
  */
 public class DelayQueueMain {
     public static void main(String[] args) {
-        DelayQueue<DelayedObj> delayQueue=new DelayQueue<DelayedObj>();
-        delayQueue.add(new DelayedObj());
-        DelayedObj obj1 = delayQueue.poll();
+        //ArrayList<String> list = Lists.newArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+        //ArrayList<String> list = Lists.newArrayList("1");
+        ArrayList<String> list = Lists.newArrayList("1", "2", "3");
+        ArrayList<Integer> list2 = Lists.newArrayList(1,2,3);
+        ArrayList<String> list1 = safeSublist(list, 2, 0);
+        ArrayList<String> strings = safeSublist(list, 2, 0);
+
     }
 
-
-    static class DelayedObj implements Delayed{
+    static class DelayedObj implements Delayed {
         @Override
         public long getDelay(TimeUnit unit) {
             return 0;
@@ -25,5 +32,26 @@ public class DelayQueueMain {
         public int compareTo(Delayed o) {
             return 0;
         }
+    }
+
+    public static <T> ArrayList<T> safeSublist(List<T> srcList, int pageSize, int page) {
+        if (CollectionUtils.isEmpty(srcList) || pageSize == 0 || page < 1) {
+            return Lists.newArrayList();
+        }
+        ArrayList<T> list = Lists.newArrayListWithExpectedSize(pageSize);
+        int total = srcList.size();
+        int start = (page - 1) * pageSize;
+        int end = start + pageSize;
+        if (end > total) {
+            end = total;
+        }
+        for (int i = start; i < end; i++) {
+            list.add(srcList.get(i));
+        }
+        System.out.println(
+            "page=" + page + " pageSise=" + pageSize + " total=" + srcList.size() + " start=" + start + " end=" + end
+                + " rs=" + list);
+
+        return list;
     }
 }
